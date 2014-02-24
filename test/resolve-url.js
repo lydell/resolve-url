4f1a8d100e177aa23731b2c7555841c7336b3d13
Simon Lydell <simon.lydell@gmail.com>
2014-02-24 20:32:21 +0100

    Switch from mocha+chai to tape for tests
    Felt like trying it out. Moreover, apparently chai supports only IE9+,
    but IE8 support is wanted.

--- test/resolve-url.js
+++ test/resolve-url.js
@@ -1,41 +1,37 @@
 // Copyright 2014 Simon Lydell
 
-var url    = require("url")
-var expect = require("chai").expect
+var url  = require("url")
+var test = require("tape")
 
 var resolveUrl = require("../")
 
-function testResolve() {
-  var expected = window.location.toString()
-  for (var index = 0, length = arguments.length; index < length; index++) {
-    expected = url.resolve(expected, arguments[index])
-  }
-  expect(resolveUrl.apply(null, arguments))
-    .to.equal(expected)
-}
-
 "use strict"
 
-describe("resolveUrl", function() {
+test("resolveUrl", function(t) {
 
-  it("is a function", function() {
-    expect(resolveUrl)
-      .to.be.a("function")
-  })
+  t.plan(7)
 
+  t.equal(typeof resolveUrl, "function", "is a function")
 
-  it("resolves like Node.js’ `url.resolve` would in series", function() {
-    testResolve()
+  // Resolve like Node.js’ `url.resolve` would in series
+  function testResolve() {
+    var expected = window.location.toString()
+    for (var index = 0, length = arguments.length; index < length; index++) {
+      expected = url.resolve(expected, arguments[index])
+    }
+    t.equal(resolveUrl.apply(null, arguments), expected, arguments.length)
+  }
 
-    testResolve("remove")
+  testResolve()
 
-    testResolve("/static/scripts/app.js")
+  testResolve("remove")
 
-    testResolve("/static/scripts/app.js", "../source-maps/app.js.map")
+  testResolve("/static/scripts/app.js")
 
-    testResolve("/static/scripts/app.js", "../source-maps/app.js.map", "../coffee/app.coffee")
+  testResolve("/static/scripts/app.js", "../source-maps/app.js.map")
 
-    testResolve("//cdn.example.com/jquery.js")
-  })
+  testResolve("/static/scripts/app.js", "../source-maps/app.js.map", "../coffee/app.coffee")
+
+  testResolve("//cdn.example.com/jquery.js")
 
 })
